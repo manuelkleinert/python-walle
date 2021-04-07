202316245cff5e20e86720325df9eca47945bad7
manuelkleinert <manuel.kleinert@gmail.com>
2021-04-07 18:49:47 +0200

    save serial

--- main.py
+++ main.py
@@ -15,54 +15,29 @@ class serialReader(Thread):
         while self.running:
             print(self.connection.read())
             sleep(.01)
-            # self.connection.flush()
     def stop(self):
         self.running = False
 
 class serialWrite(Thread):
-    def __init__(self, connection, servoSelect, servoInt, servoSpeed, *args):
+    def __init__(self, connection, *args):
         Thread.__init__(self)
         self.running = True
         self.connection = connection
-        self.servoSelect = servoSelect
-        self.servoInt = servoInt
-        self.servoSpeed = servoSpeed
+        self.serialData = []
     def run(self):
-
-        self.serialData = {
-                "servos" : [{
-                    "pin": 4,
-                    "pos": 500,
-                    "speed": 1, 
-                },{
-                    "pin": 5,
-                    "pos": 500,
-                    "speed": 1, 
-                },{
-                    "pin": 6,
-                    "pos": 500,
-                    "speed": 1, 
-                },{
-                    "pin": 0,
-                    "pos": 500,
-                    "speed": 1, 
-                },{
-                    "pin": 1,
-                    "pos": 500,
-                    "speed": 1, 
-                }]
-            }
-
         while self.running:
-            # self.json = json.dumps(self.serialData)
-            # print('SEND Now: ' + self.json)
+            if not self.serialData:
+                continue;
+                
+            for obj in self.serialData:
+                if (not self.connection.write(json.dumps(obj))):
+                    print('SEND False')
+                    break;
+                sleep(0.1)
 
-            # if (not self.connection.write(self.json)):
-               # print('SEND False')
-      
-            if (not self.connection.write("{\"pin\":" + self.servoSelect.get() + ",\"pos\":" + self.servoInt.get() +",\"speed\":" + self.servoSpeed.get() +" }")):
-                print('SEND False')
-            sleep(2)
+    def setData(self, serialData = []):
+        self.serialData = serialData
+    
     def stop(self):
         self.running = False
 
@@ -84,11 +59,27 @@ class Application(Frame):
         self.servoSpeed.setValue(1)
         self.servoSpeed.pack()
 
-        self.serialWriteThread = serialWrite(self.connection, self.servoSelect, self.servoInt, self.servoSpeed)
+        self.serialWriteThread = serialWrite(self.connection)
         self.serialWriteThread.start()
 
         self.serialReaderThread = serialReader(self.connection)
         self.serialReaderThread.start()
+
+        self.serialWriteThread.setData([
+            {
+                'pin': 4,
+                'pos': 500,
+                'speed': 1
+            },{
+                'pin': 5,
+                'pos': 500,
+                'speed': 3
+            },{
+                'pin': 6,
+                'pos': 500,
+                'speed': 2
+            }
+        ])
 
 
 app = Application(Tk())
